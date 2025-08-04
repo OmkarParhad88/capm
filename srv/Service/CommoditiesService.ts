1ffa6b6c61f54bc22d12763048573b654f1c1de8
Omkar_Parhad <omkarparhad7905@gmail.com>
2025-08-04 18:14:11 +0530

    tye to get data from anoterh odata service

--- srv/Service/CommoditiesService.ts
+++ srv/Service/CommoditiesService.ts
@@ -2,15 +2,25 @@ import {
   Request,
   ServiceLogic,
 } from '@dxfrontier/cds-ts-dispatcher';
+import cds from '@sap/cds';
 
 import type { Commodities } from '#cds-models/DemoService';
 @ServiceLogic()
 export default class CommoditiesService {
-  public showConsoleLog() {
-    console.log('****************** Before read event');
+  public async showConsoleLog() {
+    const Admin = await cds.connect.to('Admin');
+    let result = await Admin.send({
+      method: 'GET',
+      path: '/books',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+    })
+    console.log('****************** After read event');
+    console.log(result);
   }
 
   public validateData(result: Commodities, req: Request) {
-    
+
   }
 }
